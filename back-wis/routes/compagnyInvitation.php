76e94ee18dc205ed85093d4573579c5628a6f3e2
49maodo <maodo9511@gmail.com>
2025-11-29 20:01:01 +0000

    feat: add endpoint to retrieve user's invitations and improve invitation listing

--- back-wis/routes/compagnyInvitation.php
+++ back-wis/routes/compagnyInvitation.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CompanyInvitationController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/company/invitations', [CompanyInvitationController::class, 'index']);
+    Route::get('/company/my-invitations', [CompanyInvitationController::class, 'myInvitations']);
     Route::post('/company/invitations', [CompanyInvitationController::class, 'store']);
     Route::post('/company/invitations/{id}/resend', [CompanyInvitationController::class, 'resend']);
     Route::post('/invitations/{token}/accept', [CompanyInvitationController::class, 'acceptInvitation']);
