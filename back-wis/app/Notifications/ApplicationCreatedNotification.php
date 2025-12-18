995b3fb63fb92c8d299270ca509caf8dc99c4be9
49maodo <maodo9511@gmail.com>
2025-12-18 00:00:22 +0000

    feat: enable real-time broadcasting for application creation notifications

--- back-wis/app/Notifications/ApplicationCreatedNotification.php
+++ back-wis/app/Notifications/ApplicationCreatedNotification.php
@@ -3,14 +3,14 @@
 namespace App\Notifications;
 
 use App\Models\Application;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ApplicationCreatedNotification extends Notification implements ShouldQueue
+class ApplicationCreatedNotification extends Notification implements ShouldQueue, ShouldBroadcast
 {
     use Queueable;
 
